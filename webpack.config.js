0a8394d72aef09b8ccd467a644475d1fa95a93f4
Luciano Oliveira <luciano101@msn.com>
2017-12-30 13:16:44 -0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './ex1/index.jsx',
+  output: {
+  path: __dirname + '/public',
+  filename: './app.js'
+ },
+ devServer: {
+  port: 8080,
+  contentBase: './public',
+ },
+ resolve: {
+  extensions: ['.js', '.jsx'],
+  alias: {
+    modules: __dirname + '/node_modules',
+    jquery: 'modules/jquery/dist/jquery.min.js',
+    bootstrap: 'modules/bootstrap/dist/js/bootstrap.js'
+  }
+ },
+ plugins: [
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery'
+  }),
+  new ExtractTextPlugin('app.css')
+ ],
+ module: {
+  rules: [
+    {
+      test: /.js[x]?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015', 'stage-0', 'react'],
+        plugins: ['transform-object-rest-spread']
+      }
+    },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader']
+      })
+    },
+      {
+        test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+}
