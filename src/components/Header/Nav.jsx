77182db7ac7dff2dc7bed57c3ac2aae76244eeb7
Luciano Oliveira <luciano101@msn.com>
2017-12-31 23:15:06 -0800

    add menu items to nav

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
 import './Nav.css';
 
 class Nav extends Component {
   render(){
     return (
-      <div>
-        navigation
-      </div>
+      <Menu>
+        <Menu.Menu>
+          <Menu.Item>
+            Harvard Realty
+          </Menu.Item>
+        </Menu.Menu>
+        <Menu.Menu position="right">
+          <Menu.Item>
+            Sign Up
+          </Menu.Item>
+          <Menu.Item>
+            Login
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
     )
   }
 }
